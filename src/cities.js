35ee627ffc108dccbf8ea406718fa4b8f8e399f6
Pol <polbene95@gmail.com>
2018-12-05 15:29:23 +0100

    Filtering fucntion work in progress

--- src/cities.js
+++ src/cities.js
@@ -6,23 +6,51 @@ class CitiesList extends React.Component {
     constructor(props) {
         super(props)
         console.log(this.props)
+        this.state = {
+            allCities: this.props.cities,
+            cities: this.props.cities,
+            inputValue: ""
+        }
     }
     render() {
 
         
         return(
-        <div className="cityList">
-            {this.props.cities.map(city => this.createCityNode({name: city.name, src: city.src}))}
+            <div>
+            <div><input value={this.state.inputValue} onChange={this.filterCitiesByInput}></input></div>
+            <div className="cityList">
+                {this.state.cities.map(city => this.createCityNode({name: city.name, src: city.src}))}
+            </div>
         </div>
         )
         
         
     }
 
+
+
+    filterCitiesByInput(ev) {
+        this.updateInputValue(ev)
+        let array = this.state.allCities.forEach(city => {
+            if (city.nametoLowerCase().indexOf(this.state.inputValue) !== -1 || 
+            city.country.toLowerCase().indexOf(this.state.inputValue) !== -1 ||
+            this.state.inputValue === "") {
+                return city;
+            }
+        })
+        this.setState({
+            cities: array
+        })
+    }
+
+    updateInputValue(event) {
+        this.setState({
+          inputValue: event.target.value
+        });
+    }
+
     componentDidMount() {
         this.props.fetchData();
-
-        
     }
 
     createCityNode(props) {
